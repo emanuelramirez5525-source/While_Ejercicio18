ed62e6ac4f70802ac98cb7a07f6f63742ce90901
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 18:16:49 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// Calcular el factorial de un número ingresado.
 
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+int factorial = 1;
+
+while (num > 0)
+{
+    factorial *= num;
+    num--;
+}
+
+Console.WriteLine("El factorial es: "+ factorial);
